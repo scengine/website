588f376b97b3c918cedb3ad4610feda51c5e9231
Colomban Wendling <ban@Hermione.(none)>
2009-05-01 05:27:06 +0200

    Improved make_git_tar.sh utility script.
     * Safer temporary directory (using mktemp -d)
     * Search for Git first instead of failing later if not present
     * Improved error handling

--- tools/make_git_tar.sh
+++ tools/make_git_tar.sh
@@ -2,25 +2,42 @@
 
 function get_tmp_dir()
 {
-  local tmp_basedir="/tmp/"
-  local tmp_dir="$tmp_basedir/mgt_$(date +%s)"
-  
-  while ! mkdir "$tmp_dir" 2>/dev/null; do
-    tmp_dir="$tmp_basedir/mgt_$(date +%s)"
-  done
-  
-  echo "$tmp_dir"
+#  local tmp_basedir="/tmp/"
+#  local tmp_dir="$tmp_basedir/mgt_$(date +%s)"
+#  
+#  while ! mkdir "$tmp_dir" 2>/dev/null; do
+#    tmp_dir="$tmp_basedir/mgt_$(date +%s)"
+#  done
+#  
+#  echo "$tmp_dir"
+  mktemp -d -t 'mgt_XXXXXX'
 }
 
 function failed()
 {
-  echo "failed!" >&2
+  echo -n "failed" >&2
+  [ ! -z "$1" ] && echo ": $1" >&2 || echo "!" >&2
+  return 1
   #exit 1
 }
+
+function have()
+{
+  local path
+  path="$(which "$1")"
+  [ $? = 0 ] || return 1
+  [ -x "$path" ] || return 1
+  return 0
+}
+
 REPO="git://git.tuxfamily.org/gitroot/scengine/website.git"
-TEMPDIR="$(get_tmp_dir)"
+TEMPDIR="$(get_tmp_dir)" || failed "failed to create temp dir" || exit 1
 NAME="bse_git$(date '+%Y%m%d')"
 TARNAME="$NAME.tar.gz"
+
+GIT="git"
+
+have "$GIT" || failed "Git not found or not usable (GIT='$GIT')" || exit 1
 
 if [ $# -gt 0 ]; then
   OUT="$1"
@@ -33,7 +50,7 @@ fi
 
 cd "$TEMPDIR" && (
   echo "Fetching repository..."
-  git clone "$REPO" "$NAME" && (
+  "$GIT" clone "$REPO" "$NAME" && (
     echo "done."
     echo "Creating tar..."
     tar -czv --exclude '.git' -f "$TARNAME" "$NAME" && (
@@ -44,7 +61,7 @@ cd "$TEMPDIR" && (
         || failed
     ) || failed
   ) || failed
-)
+) || failed
 
 echo "Removing temp directory $TEMPDIR..."
 rm -fr "$TEMPDIR" && echo "done." || failed
