8c22b28a21c9583d58fe86a4d4de39ea98a0edde
Colomban Wendling <ban@herbesfolles.org>
2012-03-07 03:53:53 +0100

    Rewrite downloads page using templates and give it a lift

--- downloads.php
+++ downloads.php
@@ -25,138 +25,46 @@ require_once ('lib/UrlTable.php');
 require_once ('lib/User.php');
 require_once ('lib/medias.php');
 require_once ('lib/misc.php');
+require_once ('lib/PHPTemplate.php');
 
 
-function print_downloads ()
+class DownloadsTemplate extends PHPFileTemplate
 {
-	$medias = media_get_array (MediaType::RELEASE, true);
-	if (! empty ($medias))
+	public function __construct ()
 	{
-		foreach ($medias as $tag => $tagmedias)
-		{
-			if ($tag == '')
-				$tag = 'Not tagged';
-			echo '<h4 class="mediatitle">',$tag,'</h4>';
-			
-			array_multisort_2nd ($tagmedias, 'mdate', SORT_DESC);
-			
-			echo '
-			<table>
-				<tr>
-					<th>URI</th>
-					<th>Description</th>
-					<th>Size</th>
-					<th>Date</th>';
-			if (User::has_rights (ADMIN_LEVEL_MEDIA))
-			{
-				echo '
-					<th></th>
-					<th></th>';
-			}
-			echo '
-				</tr>';
-			
-			foreach ($tagmedias as $media)
-			{
-				$name = basename ($media['uri']);
-				$media['uri'] = MEDIA_DIR_R.'/'.$media['uri'];
-				
-				echo '
-				<tr>
-					<td>
-						<a href="',$media['uri'],'" title="',$media['desc'],'" class="noicon">
-							',$name,'
-						</a>
-					</td>
-					<td>
-						',$media['desc'],'
-					</td>
-					<td>
-						',get_size_string ($media['size']),'
-					</td>
-					<td>
-						',date ('d/m/Y H:i', $media['mdate']),'
-					</td>';
-				if (User::has_rights (ADMIN_LEVEL_MEDIA))
-				{
-					echo '
-					<td>
-						<a href="',UrlTable::admin_medias ('edit', $media['id']),'"
-						   title="Edit">
-							<img src="styles/',STYLE,'/edit.png" alt="Edit" />
-						</a>
-					</td>
-					<td>
-						<a href="',UrlTable::admin_medias ('rm', $media['id']),'"
-						   title="Delete">
-							<img src="styles/',STYLE,'/delete.png" alt="Delete" />
-						</a>
-					</td>';
-				}
-				echo '
-				</tr>';
-			}
-			
-			echo '
-			</table>';
-		}
+		parent::__construct ('views/downloads.phtml');
+		$this->is_admin = User::has_rights (ADMIN_LEVEL_MEDIA);
+		$this->medias = $this->get_medias ();
 	}
-	/* no media selected */
-	else
+	
+	private function get_medias ()
 	{
-		echo '<p>This section has no media.</p>';
+		$medias = media_get_medias (
+			array (MediaType::RELEASE),
+			array (),
+			array (
+				'mdate' => 'DESC',
+				'desc'  => 'ASC'
+			)
+		);
+		/* Adjust medias */
+		foreach ($medias as &$media) {
+			if ($media['desc'] == '') {
+				$media['desc'] = 'No description';
+			}
+			$media['uri'] = MEDIA_DIR_R.'/'.$media['uri'];
+			
+			/* FIXME: should the date & size formatting be done here rather than in
+			 * the template? */
+		}
+		
+		return $medias;
 	}
 }
 
 
+$tpl = new DownloadsTemplate ();
+
 require_once ('include/top.minc');
-
-?>
-
-<div id="presentation">
-	<h2><?php echo TITLE; ?></h2>
-	<p>
-		<span class="u">Note:</span> the sources are no longer released
-    as versionned archives. Instead, you can get the latest sources through our
-    git repositories.
-	</p>
-</div>
-
-<div id="content">
-	<h3>Development version</h3>
-	<p>
-	  The latest version of the engine can be retrieved using these
-    repositories:
-	</p>
-	<pre>git clone git://gitorious.org/scengine/utils.git
-git clone git://gitorious.org/scengine/core.git
-git clone git://gitorious.org/scengine/renderer-gl.git
-git clone git://gitorious.org/scengine/interface.git</pre>
-    <p>Mirror (not as much up-to-date as the previous one):</p>
-	<pre>git clone git://git.tuxfamily.org/gitroot/scengine/utils.git
-git clone git://git.tuxfamily.org/gitroot/scengine/core.git
-git clone git://git.tuxfamily.org/gitroot/scengine/renderergl.git
-git clone git://git.tuxfamily.org/gitroot/scengine/interface.git</pre>
-	<p>
-		Gitorious wiki may give you additional information:
-		<a title="Gitorious Wiki page" href="https://gitorious.org/scengine/pages/Home">
-https://gitorious.org/scengine/pages/Home</a>
-	</p>
-
-	<h3>Released versions</h3>
-	<?php
-		if (User::has_rights (ADMIN_LEVEL_MEDIA))
-		{
-			echo '
-			<div>
-				',print_button ('Add a file', UrlTable::admin_medias ('new')),'
-			</div>';
-		}
-		
-		print_downloads ();
-	?>
-		
-</div> <!-- content ends -->
-
-<?php
+$tpl->render ();
 require_once ('include/bottom.minc');
